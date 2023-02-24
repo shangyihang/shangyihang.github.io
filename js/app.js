b899a1001952a493881c3b74b722c9a686ca9642
shangyihang <121275868+shangyihang@users.noreply.github.com>
2023-02-24 08:58:13 +0800

    dsa
    sdadas

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ const App = () => {
   let [face, setFace] = useState('');
   let [num, setNum] = useState(30);
   let [faces] = useState([
-    'http://iwenwiki.com/api/livable/homehot/img_chuwugui.png',
+    '../head_pic/user1.jpg',
     'http://iwenwiki.com/api/livable/homehot/img_jingzi.png',
     'http://iwenwiki.com/api/livable/homehot/img_luodideng.png',
     'http://iwenwiki.com/api/livable/homehot/img_maojin.png',
